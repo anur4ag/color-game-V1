d41ad0941766a583637be051595734e182ab3642
shanurag31 <71564387+shanurag31@users.noreply.github.com>
2020-10-12 15:22:50 +0530

    commented codes are deleted

--- Color game/colorGame.js
+++ Color game/colorGame.js
@@ -40,36 +40,6 @@ function reset(){
     h1.style.backgroundColor = "rgb(34, 120, 201)";
 }
 
-// easyBtn.addEventListener("click",function(){
-//     easyBtn.classList.add("selected")
-//     hardBtn.classList.remove("selected");
-//     numberSquares = 3
-//     colors = generateRandomColors(numberSquares);
-//     pickedColor = pickColor();
-//     colorDisplay.textContent = pickedColor;
-//     for (var i = 0 ; i < squares.length ; i++){
-//         if(colors[i]){
-//             squares[i].style.backgroundColor = colors[i];
-//         }else {
-//             squares[i].style.display = "none";
-//         }
-//     }
-// });
-// hardBtn.addEventListener("click",function(){
-//     hardBtn.classList.add("selected");
-//     easyBtn.classList.remove("selected");
-//     numberSquares = 6
-//     colors = generateRandomColors(numberSquares);
-//     pickedColor = pickColor();
-//     colorDisplay.textContent = pickedColor;
-//     for (var i = 0 ; i < squares.length ; i++){
-//         squares[i].style.backgroundColor = colors[i];
-//         squares[i].style.display = "block";
-//     }
-
-// });
-
-
 resetButton.addEventListener("click", function(){
     reset();
 })
